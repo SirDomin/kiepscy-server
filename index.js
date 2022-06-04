56e3b85989e0ccef82d86dd3b7fe14152ae455e3
SirDomin <dominik.garbulski@gmail.com>
2022-06-04 21:30:16 +0200

    [Sound] add delay

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import exec from 'child_process'
 
 import http from 'http'
 
+let lastTriggered = Date.now();
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
-    playSound()
+    if (Date.now() - lastTriggered > 3000) {
+        playSound()
+        lastTriggered = Date.now();
+    }
 
     res.write('Hello World!');
     res.end();
